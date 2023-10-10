a2d7a80c2bc2b7cccefc9e6e8ee468169ace8220
maneeshgujar <maneeshgujar634@gmail.com>
2023-10-11 02:18:57 +0530

    commit

--- firstproj/settings.py
+++ firstproj/settings.py
@@ -140,12 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR / 'productionfiles'
+STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
-MEDIA_URLS='/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+# MEDIA_URLS='/media/'
+# MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 
 STATICFILES_DIRS = [
