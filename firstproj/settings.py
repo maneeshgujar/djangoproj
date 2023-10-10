13dbcb0887ce78f4d0974d0537760e55b1cc47af
maneeshgujar <maneeshgujar634@gmail.com>
2023-10-11 02:11:29 +0530

    commit

--- firstproj/settings.py
+++ firstproj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,9 +140,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'productionfiles'
+# STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+MEDIA_URLS='/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+
 
 STATICFILES_DIRS = [
     BASE_DIR / 'mystaticfiles'
